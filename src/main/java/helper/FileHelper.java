65b0fb84c070e440d0553739e9828b15e903b230
vevdokimenko <evdokimenko@hotmail.com>
2022-05-17 10:12:11 +0300

    Working filepath fixes

--- src/main/java/helper/FileHelper.java
+++ src/main/java/helper/FileHelper.java
@@ -1,19 +1,29 @@
 package helper;
 
-import lombok.AllArgsConstructor;
+import java.io.*;
+import java.util.Arrays;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
-@AllArgsConstructor
 public class FileHelper {
-    private String inputFilePath;
-    private String outputFilePath;
+    private final String inputFilePath;
+    private final String outputFilePath;
+    private final String dir;
+
+    public FileHelper(String inputFilePath, String outputFilePath) {
+        this.inputFilePath = inputFilePath;
+        this.outputFilePath = outputFilePath;
+        this.dir = findInputFileDir();
+    }
+
+    private String findInputFileDir() {
+        return
+                (System.getProperty("user.dir") != null)
+                        ? System.getProperty("user.dir").concat("/")
+                        : null;
+    }
 
     public String getFileContents() {
         StringBuilder stringBuilder = new StringBuilder();
-        try (FileReader fileReader = new FileReader(inputFilePath)) {
+        try (FileReader fileReader = new FileReader(dir + inputFilePath)) {
             while (fileReader.ready()) {
                 int value = fileReader.read();
                 stringBuilder.append((char) value);
@@ -25,7 +35,7 @@ public class FileHelper {
     }
 
     public void writeOutputFile(String contents) {
-        try (FileWriter fileWriter = new FileWriter(outputFilePath)) {
+        try (FileWriter fileWriter = new FileWriter(dir + outputFilePath)) {
             fileWriter.write(contents);
         } catch (IOException e) {
             e.printStackTrace();
